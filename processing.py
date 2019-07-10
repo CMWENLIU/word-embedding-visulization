fed8987e86a1ed390dd81952a3ff1ac75a0fd184
bear <bear@localhost.localdomain>
2019-07-10 11:14:21 -0500

    fix error

--- processing.py
+++ processing.py
@@ -16,10 +16,10 @@ def remove_stopwords(s):
       filtered_sentence.append(w) 
   return ' '.join(filtered_sentence)
 i = 1
-with open(os.path.join('/home/bear/bigdata/', 'tox30000.txt'), 'w') as wf:
+with open(os.path.join('/home/bear/bigdata/', 'tox.txt'), 'w') as wf:
  with open('/home/bear/bigdata/tox_allinfor.txt', 'r') as rf:
   for line in rf:
-   if '-##-' in line and i < 30000:
+   if '-##-' in line:
     s = line.split('-##-', 1)[1].replace('\n', '').lower()
     s = s.translate(str.maketrans('', '', string.punctuation))
     s = remove_stopwords(s)
